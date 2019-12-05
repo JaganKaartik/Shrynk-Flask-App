5fc362bf035dcedda22c39d5b6cce83bd232c891
JaganKaartik <kaartikjagan@gmail.com>
2019-12-06 00:38:44 +0530

    Added Login/Register Functionality and Basic Routing

--- shrynk/__init__.py
+++ shrynk/__init__.py
@@ -1,6 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'N2QRzZW7OS'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shrynk.db'
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
 from shrynk import routes
 
--- shrynk/routes.py
+++ shrynk/routes.py
@@ -1,6 +1,48 @@
-from shrynk import app
-from flask import render_template
+from shrynk import app, db, bcrypt
+from flask import render_template, flash, redirect, url_for
+from shrynk.forms import RegistrationForm, LoginForm
+from flask_login import login_user, current_user, logout_user, login_required
+from shrynk.models import User
 
 @app.route("/")
-def home():
+def mainPage():
     return render_template('layout.html')
+
+@app.route("/home")
+def home():
+    return render_template('home.html')
+
+@app.route("/login", methods=['GET','POST'])
+def loginUser():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user)
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form = form)
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def registerUser():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, password=hashed_password) 
+        db.session.add(user)
+        db.session.commit()
+        flash(f'Account created for {form.username.data}!', 'success!')
+        return redirect(url_for('loginUser'))
+    return render_template('register.html', title='Register', form = form)
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('mainPage'))
