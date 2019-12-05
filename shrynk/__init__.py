25b8c522ba6b18f9672a078bba7c444c9fb4e9fe
JaganKaartik <kaartikjagan@gmail.com>
2019-12-06 00:49:39 +0530

    First Database Migration

--- shrynk/__init__.py
+++ shrynk/__init__.py
@@ -2,15 +2,18 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'N2QRzZW7OS'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shrynk.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
+migrate = Migrate(app, db)
 
 from shrynk import routes
 
