0c63c2ca5eb6e25312eecf73d7ecd775e45e2b97
JaganKaartik <kaartikjagan@gmail.com>
2021-03-12 03:34:02 +0530

    Minor Refactor

--- shrynk/__init__.py
+++ shrynk/__init__.py
@@ -8,7 +8,7 @@ from flask_admin import Admin
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['SQLALCHEMY_DATABASE_URI']
+app.config['DATABASE_URL'] = os.environ['DATABASE_URL']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
